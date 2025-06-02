c3c0d2b4452243758a13815fa4928060e4561ff1
iosses <oss24002@byui.edu>
2025-06-02 09:36:07 -0600

    Everything working ok. before assignment 3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,4 +6,7 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
+// Vehicle detail route
+router.get("/detail/:inv_id", invController.buildDetailView);
+
 module.exports = router;
