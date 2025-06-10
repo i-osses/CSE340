1f0a619faf322466e0b238f92ca29ef0daea0320
iosses <oss24002@byui.edu>
2025-06-10 07:17:34 -0600

    W4 Act1 Sessions and Messages

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,6 +4,7 @@ const baseController = {}
 baseController.buildHome = async function(req, res){
   try {
     const nav = await utilities.getNav()
+    // req.flash("notice", "This is a flash message.")
     res.render("index", { title: "Home", nav })
   } catch (err) {
     next(err)
