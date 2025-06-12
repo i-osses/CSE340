7c51bd8b5eb6ff0e6b7abb7aac50175ce540f5e1
iosses <oss24002@byui.edu>
2025-06-12 03:04:09 -0600

    Unit 4 -Password Hashing

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,7 @@
 // Unit 4, deliver login view activity
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 /* ***********************
  * deliver login view activity
@@ -44,11 +45,31 @@ async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+
+  // Hash the password before storing
+let hashedPassword
+try {
+  // regular password and cost (salt is generated automatically)
+  hashedPassword = await bcrypt.hash(account_password, 10)
+} catch (error) {
+  req.flash("notice", 'Sorry, there was an error processing the registration.')
+  return res.status(500).render("account/register", {
+    title: "Registration",
+    nav,
+    errors: null,
+    account_firstname,
+    account_lastname,
+    account_email
+  })
+}
+
+
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -65,9 +86,13 @@ async function registerAccount(req, res) {
 
   } else {
     req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
+     res.status(501).render("account/register", {
       title: "Registration",
       nav,
+      errors: null,
+      account_firstname,
+      account_lastname,
+      account_email
     })
   }
 }
